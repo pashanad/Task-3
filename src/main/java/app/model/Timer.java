c7884ef6d69e279bcfad128dc13beddea92cbf61
Pavel Nadareyshvili <pasha.nad@mail.ru>
2025-04-01 15:20:39 +0300

    first commit

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,11 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
